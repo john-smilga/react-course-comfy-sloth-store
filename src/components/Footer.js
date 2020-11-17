ce240ab25403abe399b72e1d9410bcf3b15b254d
john-smilga <johnsmilga@gmail.com>
2020-11-17 13:26:24 -0800

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,11 @@ import styled from 'styled-components'
 const Footer = () => {
   return (
     <Container>
-      <div>
-        <h4>
-          &copy; {new Date().getFullYear()}
-          <span> ComfySloth </span>
-        </h4>
-        <h4>All rights reserved</h4>
-      </div>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> ComfySloth </span>
+      </h5>
+      <h5>All rights reserved</h5>
     </Container>
   )
 }
@@ -17,19 +15,24 @@ const Footer = () => {
 const Container = styled.footer`
   height: 5rem;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   background: var(--clr-black);
   text-align: center;
   span {
     color: var(--clr-primary-5);
   }
-  h4 {
+  h5 {
     color: var(--clr-white);
-    margin-bottom: 0;
+    margin: 0.1rem;
+
     font-weight: 400;
     text-transform: none;
     line-height: 1.25;
+  }
+  @media (min-width: 776px) {
+    flex-direction: row;
   }
 `
 
--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 const ProductImages = ({ images = [[]] }) => {
   const [main, setMain] = useState(images[0])
   return (
