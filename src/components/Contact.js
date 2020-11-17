f7552208f186d52ff7be5f772a6e09090d25279b
john-smilga <johnsmilga@gmail.com>
2020-11-17 11:07:03 -0800

    contact form complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,23 @@ const Contact = () => {
     <Wrapper>
       <div className='section-center'>
         <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            sint unde quaerat ratione soluta veniam provident adipisci cumque
+            eveniet tempore?
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
       </div>
     </Wrapper>
   )
@@ -13,6 +30,60 @@ const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
     text-transform: none;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    color: var(--clr-grey-5);
+  }
+  .contact-form {
+    width: 500px;
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+
+  .form-input,
+  .submit-btn {
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+    border: 2px solid var(--clr-black);
+  }
+  .form-input {
+    border-right: none;
+    color: var(--clr-grey-3);
+    border-top-left-radius: var(--radius);
+    border-bottom-left-radius: var(--radius);
+  }
+  .submit-btn {
+    border-top-right-radius: var(--radius);
+    border-bottom-right-radius: var(--radius);
+  }
+  .form-input::placeholder {
+    color: var(--clr-black);
+    text-transform: capitalize;
+  }
+  .submit-btn {
+    background: var(--clr-primary-5);
+    text-transform: capitalize;
+    letter-spacing: var(--spacing);
+    cursor: pointer;
+    transition: var(--transition);
+    color: var(--clr-black);
+  }
+  .submit-btn:hover {
+    color: var(--clr-white);
+  }
+  @media (min-width: 992px) {
+    .content {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      gap: 8rem;
+      margin-top: 2rem;
+    }
+    p {
+      margin-bottom: 0;
+    }
   }
   @media (min-width: 1280px) {
     padding: 15rem 0;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,13 @@ import styled from 'styled-components'
 const Footer = () => {
   return (
     <Container>
-      <h4>
-        &copy; {new Date().getFullYear()}
-        <span> ComfySloth </span> All rights reserved
-      </h4>
+      <div>
+        <h4>
+          &copy; {new Date().getFullYear()}
+          <span> ComfySloth </span>
+        </h4>
+        <h4>All rights reserved</h4>
+      </div>
     </Container>
   )
 }
@@ -17,6 +20,7 @@ const Container = styled.footer`
   align-items: center;
   justify-content: center;
   background: var(--clr-black);
+  text-align: center;
   span {
     color: var(--clr-primary-5);
   }
@@ -25,6 +29,7 @@ const Container = styled.footer`
     margin-bottom: 0;
     font-weight: 400;
     text-transform: none;
+    line-height: 1.25;
   }
 `
 
